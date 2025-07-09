5bcd163f139f6d10e147d9d0ae8d9372a324580b
Brunno <codgoblue@gmail.com>
2025-07-09 19:48:50 -0300

    Resolvendo conflitos

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const sequelize = require('./server/config/config_db.js');
 // const router = require("./server/router/router.js") trocar para o arquivo que usarem
 
 app.use(express.json())
-app.use(express.static("clqient/src"))
+app.use(express.static("client/src"))
 app.use("/node_modules", express.static("node_modules"))
 
 // app.use(router) depois que criarem o arquivo de rotas
